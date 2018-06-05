c6d4b1c208414bbbcebc4dfb5acb3df9bc47a0b5
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2018-06-05 10:34:56 -0500

    Modify content and styles for GitHub Pages

--- docs/assets/scripts/app.js
+++ docs/assets/scripts/app.js
@@ -10345,31 +10345,19 @@ function openNav() {
   // Site Navigation
   $("#site-navigation").addClass("site-header__navigation--open");
   $(".site-header__nav-item").addClass("site-header__nav-item--open");
-  //document.getElementById("site-navigation").style.width = "12.5%";
   // Hero Section
   $("#hero-section").addClass("nav--open");
-  //document.getElementById("hero-section").style.width = "87.5%";
-  //document.getElementById("hero-section").style.marginLeft = "12.5%";
   // About Section
   $("#about-section").addClass("nav--open");
-  //document.getElementById("about-section").style.width = "87.5%";
-  //document.getElementById("about-section").style.marginLeft = "12.5%";
   // Skills Section
   $("#skills-section").addClass("nav--open");
-  //document.getElementById("skills-section").style.width = "87.5%";
-  //document.getElementById("skills-section").style.marginLeft = "12.5%";
   // Portfolio Section
   $("#portfolio-section").addClass("nav--open");
-  //document.getElementById("portfolio-section").style.width = "87.5%";
-  //document.getElementById("portfolio-section").style.marginLeft = "12.5%";
   // Contact Section
   $("#contact-section").addClass("nav--open");
-  //document.getElementById("contact-section").style.width = "87.5%";
-  //document.getElementById("contact-section").style.marginLeft = "12.5%";
   // Footer
   $("#footer").addClass("nav--open");
-  //document.getElementById("footer").style.width = "87.5%";
-  //document.getElementById("footer").style.marginLeft = "12.5%";
+  // Navigation is open
   navOpen = true;
 }
 
@@ -10378,31 +10366,19 @@ function closeNav() {
   // Site navigation
   $("#site-navigation").removeClass("site-header__navigation--open");
   $(".site-header__nav-item").removeClass("site-header__nav-item--open");
-  //document.getElementById("site-navigation").style.width = "0";
   // Hero Section
   $("#hero-section").removeClass("nav--open");
-  //document.getElementById("hero-section").style.width = "100%";
-  //document.getElementById("hero-section").style.marginLeft = "0";
   // About Section
   $("#about-section").removeClass("nav--open");
-  //document.getElementById("about-section").style.width = "100%";
-  //document.getElementById("about-section").style.marginLeft = "0%";
   // Skills Section
   $("#skills-section").removeClass("nav--open");
-  //document.getElementById("skills-section").style.width = "100%";
-  //document.getElementById("skills-section").style.marginLeft = "0%";
   // Portfolio Section
   $("#portfolio-section").removeClass("nav--open");
-  //document.getElementById("portfolio-section").style.width = "100%";
-  //document.getElementById("portfolio-section").style.marginLeft = "0%";
   // Contact Section
   $("#contact-section").removeClass("nav--open");
-  //document.getElementById("contact-section").style.width = "100%";
-  //document.getElementById("contact-section").style.marginLeft = "0%";
   // Footer
   $("#footer").removeClass("nav--open");
-  //document.getElementById("footer").style.width = "100%";
-  //document.getElementById("footer").style.marginLeft = "0%";
+  // Navigation is closed
   navOpen = false;
 }
 
@@ -10479,12 +10455,12 @@ $('a[href*="#"]')
 /* 4 */
 /***/ (function(module, exports) {
 
-// Copy e-mail address to the clipboard when corresponding icon is clicked
+// Copy e-mail address to the Clipboard when corresponding icon is clicked
 var emailAddress = document.getElementById('copyEmail');
 var clipboard = new Clipboard(emailAddress);
 clipboard.on('success', function(e) {
   console.log(e);
-  // Notify user that e-mail address has been copied to clipboard
+  // Notify user that e-mail address has been copied to the Clipboard
   (function($){
     $(function(){
   	   $.jGrowl('Copied "' + e.text + '" to the Clipboard.');
